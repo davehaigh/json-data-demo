4609e9b5cb37218487aca6f1b831419e44259402
Dave Haigh <dave.haigh@digital.hmrc.gov.uk>
2017-09-26 15:01:31 +0100

    added notes and third json file

--- app/routes.js
+++ app/routes.js
@@ -2,18 +2,20 @@ var express = require('express')
 var router = express.Router()
 
 ////////////////////////////////////////////
-// pageData object to pass to prototype versions //
+// myData object to pass to prototype versions //
 ////////////////////////////////////////////
-var pageData = {
+var myData = {
   // Fake Data JSON Files //
   accounts: {
     "account-1": require(__dirname + '/data/account-1.json'),
-    "account-2": require(__dirname + '/data/account-2.json')
+    "account-2": require(__dirname + '/data/account-2.json'),
+    "account-3": require(__dirname + '/data/account-3.json')
   }
 }
 
 //Version 1-0 routing
-require('./routes/1-0/routes.js')(router,pageData);
+//Pass myData variable as parameter into version specific routes file
+require('./routes/1-0/routes.js')(router,myData);
 
 // Route index page
 router.get('/', function (req, res) {
--- app/routes/1-0/routes.js
+++ app/routes/1-0/routes.js
@@ -1,15 +1,19 @@
-module.exports = function(router,pageData) {
+module.exports = function(router,myData) {
     
     var version = "1-0";
 
     //WILDCARD get /*
     router.get('/' + version + '/*', function (req, res, next) {
-        //Set req.session.pageData for first time or if query string of reqResetData is present
-        if(!req.session.pageData || req.query.reqResetData) {
-            req.session.pageData = pageData
-            req.session.pageData.selectedAccount = "account-1"
+        //
+        //Set req.session.myData for first time or if query string of resetSession is present
+        //
+        if(!req.session.myData || req.query.resetSession) {
+            req.session.myData = myData
+            req.session.myData.selectedAccount = "account-1"
         }
-        req.session.pageData.selectedAccount = req.query.accountID || req.session.pageData.selectedAccount
+        //Set selectedAccount id on every get in case a query string was specified
+        req.session.myData.selectedAccount = req.query.accountID || req.session.myData.selectedAccount
+        //next(); makes sure next get request in stack is hit
         next();
     });
 
@@ -18,23 +22,26 @@ module.exports = function(router,pageData) {
 
         //Render page
         res.render(version + '/index', {
-            pageData: req.session.pageData
+            myData: req.session.myData
         });
 
     });
     router.post('/' + version + '/index', function (req, res) {
 
+        //
+        //DEMO OF HOW YOU CAN EDIT THE DATA IN YOUR SESSION - AGAINST THE CORRECT ACCOUNT
+        //
         //Find selected account from your data sets
-        var _selectedAccount = req.session.pageData.accounts[req.session.pageData.selectedAccount]
+        var _selectedAccount = req.session.myData.accounts[req.session.myData.selectedAccount]
         var _selectedAccountsUsers = _selectedAccount.users
-        
+        //Edit age against each user in list
         _selectedAccountsUsers.forEach(function(user, index) {
             user.age = req.session.data.accountusers[index]
         });
         
         //Render page
         res.render(version + '/index', {
-            pageData: req.session.pageData
+            myData: req.session.myData
         });
 
     });
